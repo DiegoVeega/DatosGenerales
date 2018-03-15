aa872c3caf8d91f627dcf1cce1fadd99473e8f0b
diego <diego@DESKTOP-4LB1GM1>
2018-03-14 18:44:35 -0600

    DatosGenerales.

---
+++ src/datosgenerales/DatosGenerales.java
@@ -0,0 +1,11 @@
+package datosgenerales;
+
+/**
+ *
+ * @author diego
+ */
+public class DatosGenerales {
+    public static void main(String[] args) {
+        System.out.println("Datos generales:\n\n"+"Nombre: Diego Edgardo Vega Herrera.\n"+"Fecha de nacimiento: 29 de Septiembre de 1997.\n"+"Edad: 20 a;os.\n"+"Pais: El Salvador.\n"+"Profesion: Estudiante.\n"+"Lugar de profesion: Universidad Centroamericana Jose Simeon Ca;as.\n"+"Carrera: Ingenieria Informatica.\n");
+    }
+}
